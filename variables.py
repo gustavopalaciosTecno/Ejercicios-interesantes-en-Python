55264c573b23da6579386e5f3f1866dd69ed8e96
Nestor Gustavo <gustavopalaciosTecno@users.noreply.github.com>
2023-04-07 16:58:12 -0300

    variables.

---
+++ variables.py
@@ -0,0 +1,9 @@
+# Crear tres variables e imprimir por pantalla el tipo de dato
+
+pais = "Argentina"
+edad = 41
+superficie = 14581.4555
+
+print(type(pais))
+print(type(edad))
+print(type(superficie))
